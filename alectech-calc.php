0593731c9f25be852544791eddb30464abe83c64
AlecTech <eremeevoleg@gmail.com>
2020-10-07 14:37:03 -0600

    initial commit

---
+++ alectech-calc.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       AlecTech Calculator
+ * Plugin URI:        https://AlecTech.ca
+ * Description:       A simple PHP calc
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            Oleg Eremeev
+ * Author URI:        https://www.nedra.ca
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
